15fc2df56c3d502f3b25e8136a15e3811c402cfc
EvgenyVarlamov <varlamov-el@yandex.ru>
2022-04-24 20:45:07 +0300

    add Homework #1 Cycle of A^B calculation

---
+++ Homework_1/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задача 25. Напишите цикл, который принимает на вход
+   два числа (А и В) и вводит число А в степень В.*/
+
+Console.WriteLine("Введите число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень числа");
+int numberPow = Convert.ToInt32(Console.ReadLine());
+
+int result = 1;
+
+for(int i = 1; i <= numberPow; i++)
+{
+    result *= numberA;
+}
+
+Console.WriteLine($"{numberA}^{numberPow} = {result}");
